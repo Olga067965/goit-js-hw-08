e30e65a82ecc353cadbe8085e909369aa3ebc52a
Olga067965 <olgasavelieva5@gmail.com>
2023-07-18 17:43:26 +0300

    Upd2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,17 +5,13 @@ const player = new Player('vimeo-player');
 
 player.on('timeupdate', event => {
   const currentTime = event.seconds;
-
-  // збережено поточний час відтворення у локальному сховищі
   localStorage.setItem('videoplayer-current-time', currentTime);
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  // отримано збережений час відтворення з локального сховища
   const savedTime = localStorage.getItem('videoplayer-current-time');
 
   if (savedTime) {
-    // встановлено час відтворення плеєра зі збереженої позиції
     player.setCurrentTime(savedTime);
   }
 });
@@ -24,8 +20,6 @@ player.on(
   'timeupdate',
   throttle(event => {
     const currentTime = event.seconds;
-
-    // збережено поточний час відтворення у локальному сховищі
     localStorage.setItem('videoplayer-current-time', currentTime);
   }, 1000)
 );
